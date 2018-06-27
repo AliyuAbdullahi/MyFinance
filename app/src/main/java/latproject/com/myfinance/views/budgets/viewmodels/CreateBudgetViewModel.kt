e9ced2d18d9998c6886e88141433c49bc3023289
aliyu abdullahi <aliyu.olalekan13@gmail.com>
2018-06-27 02:58:29 +0100

    fix bug with sorting of transaction

--- app/src/main/java/latproject/com/myfinance/views/budgets/viewmodels/CreateBudgetViewModel.kt
+++ app/src/main/java/latproject/com/myfinance/views/budgets/viewmodels/CreateBudgetViewModel.kt
@@ -14,40 +14,41 @@ class CreateBudgetViewModel(context: Context) : CoreViewModel(context) {
 
     fun getBaseBalance(): Double {
         val currentBank = dataStore.getBank()
-        val base = dataStore.getAllTransactions()?.map { it.balanceAfterTransaction }?.last()
-
-//        val base = dataStore.getTransactionsForBank(currentBank!!)?.map { it.balanceAfterTransaction }?.last()
+        var base: Double? = null
+        if (currentBank != null) {
+            base = dataStore.getTransactionsForBank(currentBank)?.map { it.balanceAfterTransaction }?.first()
+        }
 
         return base ?: 0.00
     }
 
-    fun getAllBalance() : Int?  {
+    fun getAllBalance(): Int? {
         return dataStore.getAllTransactions()?.size
     }
 
     fun getLastCredit(): Double {
         val currentBank = dataStore.getBank()
 
-        val lastCredit = dataStore.getTransactionsForBank(currentBank!!)?.filter { it.isCredit }?.map { it.amount }?.last()
+        val lastCredit = dataStore.getTransactionsForBank(currentBank!!)?.filter { it.isCredit }?.map { it.amount }?.first()
 
         return lastCredit ?: 0.00
     }
 
     @Bindable
     var base: Double = 0.00
-    set(value) {
-        field = value
-        notifyPropertyChanged(BR.base)
-        notifyPropertyChanged(BR.currentBase)
-    }
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.base)
+            notifyPropertyChanged(BR.currentBase)
+        }
 
     @Bindable
-    var currentBase : String = ""
-    get() = "$base"
-    set(value) {
-        field = value
-        notifyPropertyChanged(BR.currentBase)
-    }
+    var currentBase: String = ""
+        get() = "$base"
+        set(value) {
+            field = value
+            notifyPropertyChanged(BR.currentBase)
+        }
 
     @Bindable
     var currentBank: String = ""
--- app/src/main/java/latproject/com/myfinance/views/transactions/viewmodels/TransactionsViewModel.kt
+++ app/src/main/java/latproject/com/myfinance/views/transactions/viewmodels/TransactionsViewModel.kt
@@ -7,42 +7,13 @@ import latproject.com.myfinance.core.room.RealmBankTransaction
 import latproject.com.myfinance.core.services.sms.SmsReader
 import latproject.com.myfinance.core.view.CoreViewModel
 
-class TransactionsViewModel(val context: Context) : CoreViewModel(context) {
+class TransactionsViewModel(context: Context) : CoreViewModel(context) {
 
     fun getAllTransactionsForBank(bankName: String): List<RealmBankTransaction>? {
         return dataStore.getTransactionsForBank(bankName)
     }
 
-    private fun getMessages(bankName: String): List<SmsMessage> {
-        val smsReader = SmsReader(context)
-
-        return smsReader.getMessage()
-                .filter { it.from.toLowerCase()
-                .contains(bankName.toLowerCase()
-                .substring(0, bankName.length/2)) }
-    }
-
-    fun getAllMessages(): List<SmsMessage> {
-        val smsReader = SmsReader(context)
-
-        return smsReader.getMessage()
-    }
-
-    fun getBank() : String?  {
+    fun getBank(): String? {
         return dataStore.getBank()
-    }
-
-    fun loadTransactions(bankName: String):MutableList<RealmBankTransaction?> {
-        val bankTransactions = mutableListOf<RealmBankTransaction?>()
-
-        getMessages(bankName).forEach {
-            val currentTransaction = TransactionParser.parseToTransaction(bankName, it)
-            if(currentTransaction != null) {
-                bankTransactions.add(currentTransaction)
-                dataStore.addTransaction(currentTransaction)
-            }
-        }
-
-        return bankTransactions
     }
 }
